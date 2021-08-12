616114c2a9a25ff93f14f411dbb311d265002c33
Surguchev <vladimir.surguchev@mail.ru>
2021-08-12 22:22:35 +0300

    Test

---
+++ app/src/main/java/com/example/my_first_app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.my_first_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//        EditText firstDigit =findViewById(R.id)
+//        EditText secondDigit =findViewById(R.id)
+    }
+}
