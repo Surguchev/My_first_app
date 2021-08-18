01a439915b4dad2eda1bf232fe788bc19ea61828
Surguchev <vladimir.surguchev@mail.ru>
2021-08-19 01:57:40 +0300

    Test

---
+++ app/src/main/java/com/example/my_first_app/FirstHomeworkClass.java
@@ -0,0 +1,26 @@
+package com.example.my_first_app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstHomeworkClass extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_text);
+
+        Button checkEdit = findViewById(R.id.edit_button);
+        checkEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+    }
+
+//    Когда запускаю FirstHomeworkClass то выполняется переход на прошлый экран, к когда запускаю весь app, то нет. В чем проблема ?
+}
+
--- app/src/main/java/com/example/my_first_app/MainActivity.java
+++ app/src/main/java/com/example/my_first_app/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.my_first_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,27 +21,33 @@ public class MainActivity extends AppCompatActivity {
 
         EditText firstDigit = findViewById(R.id.main_firstEdit);
         EditText secondDigit = findViewById(R.id.main_secondEdit);
-
         Button compareButton = findViewById(R.id.main_button);
-
         TextView result = findViewById(R.id.main_textView);
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch check = findViewById(R.id.main_switchButton);
+
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.edit_text);
+            }
+        });
 
         compareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               String inputOne =  firstDigit.getText().toString();
-               String inputTwo = secondDigit.getText().toString();
+                String inputOne = firstDigit.getText().toString();
+                String inputTwo = secondDigit.getText().toString();
 
-                Log.v("User input", "Entered " + inputOne + " " + inputTwo );
-               try {
+                Log.v("User input", "Entered " + inputOne + " " + inputTwo);
+                try {
 
-                   if (Integer.valueOf(inputOne).equals(Integer.valueOf(inputTwo))) {
-                       result.setText("Equal");
-                   } else {
-                       result.setText("Not equal");
-                   }
-               } catch (NumberFormatException exc){
-                   result.setText("WTF leather man ?!");
+                    if (Integer.valueOf(inputOne).equals(Integer.valueOf(inputTwo))) {
+                        result.setText("Equal");
+                    } else {
+                        result.setText("Not equal");
+                    }
+                } catch (NumberFormatException exc) {
+                    result.setText("WTF leather man ?!");
                 }
             }
         });
